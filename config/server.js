44a7699683f52e66c04d2188553bd0a37589ffc0
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2020-08-02 17:13:42 -0700

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '61993e23b79349bb04e5d970bafa8993'),
+    },
+  },
+});
